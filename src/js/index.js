c2813f791b27032d1dfb65dd17d605cc821b5afc
Daniel Howorth <daniel.howorth@sky.com>
2024-11-08 16:33:09 +0000

    added dropdown menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 import "../css/styles.css";
+
+const headerMenuBtn = document.querySelector("#header-menu-btn");
+
+headerMenuBtn.addEventListener("click", () => {
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+  dropdownMenu.style.willChange = "max-height";
+  dropdownMenu.classList.toggle("active");
+
+  dropdownMenu.addEventListener(
+    "transitionend",
+    () => {
+      dropdownMenu.style.willChange = "auto";
+    },
+    { once: true }
+  );
+});
