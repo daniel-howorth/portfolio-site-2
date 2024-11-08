7f627eb0e40ec0949eb50946330ef858787b094e
Daniel Howorth <daniel.howorth@sky.com>
2024-11-08 16:56:00 +0000

    toggle aria-expanded on dropdown toggle

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,13 @@ const headerMenuBtn = document.querySelector("#header-menu-btn");
 
 headerMenuBtn.addEventListener("click", () => {
   const dropdownMenu = document.querySelector(".dropdown-menu");
+
+  // set will-change on max-height to optimise animation
   dropdownMenu.style.willChange = "max-height";
+
   dropdownMenu.classList.toggle("active");
 
+  // unset will-change property to release GPU resources
   dropdownMenu.addEventListener(
     "transitionend",
     () => {
@@ -14,4 +18,8 @@ headerMenuBtn.addEventListener("click", () => {
     },
     { once: true }
   );
+
+  // toggle aria-expanded attribute to notify screen-readers of dropdown state
+  const isExpanded = headerMenuBtn.getAttribute("aria-expanded") === "true";
+  headerMenuBtn.setAttribute("aria-expanded", !isExpanded);
 });
