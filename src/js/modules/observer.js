b39e8ee74bf53ed2e8dc95841d1d6174cebb2d5e
Daniel Howorth <daniel.howorth@sky.com>
2025-02-19 11:09:55 +0000

    elements only fade up when they start below the viewport

--- src/js/modules/observer.js
+++ src/js/modules/observer.js
@@ -4,31 +4,55 @@ When an observed element is in view, add the in-view class.
 Stop observing once the element is in view.
 */
 document.addEventListener("DOMContentLoaded", function () {
+  // const observer = new IntersectionObserver(
+  //   (entries, observer) => {
+  //     entries.forEach((entry) => {
+  //       if (entry.isIntersecting) {
+  //         const el = entry.target;
+
+  //         // add will-change just before animation
+  //         const willChange = el.dataset.willChange;
+  //         if (willChange) {
+  //           el.style.willChange = willChange;
+  //         }
+
+  //         entry.target.classList.add("in-view");
+
+  //         // reset will-change after animation and remove listener
+  //         if (willChange) {
+  //           el.addEventListener(
+  //             "transitionend",
+  //             function handleTransitionEnd() {
+  //               el.style.willChange = "auto";
+  //               el.removeEventListener("transitionend", handleTransitionEnd);
+  //             }
+  //           );
+  //         }
+
+  //         observer.unobserve(entry.target);
+  //       }
+  //     });
+  //   },
+  //   { threshold: 0.1 }
+  // );
+
+  let initialScrollY = window.scrollY;
+  const initialFadeUpElements = document.querySelectorAll(".fade-up");
+
+  // If elements start at or above the intial scroll point, remove fade up animation.
+  initialFadeUpElements.forEach((el) => {
+    const rect = el.getBoundingClientRect();
+    const elementBottom = rect.bottom + window.scrollY;
+    if (initialScrollY >= elementBottom) {
+      el.classList.remove("fade-up");
+    }
+  });
+
   const observer = new IntersectionObserver(
     (entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          const el = entry.target;
-
-          // add will-change just before animation
-          const willChange = el.dataset.willChange;
-          if (willChange) {
-            el.style.willChange = willChange;
-          }
-
           entry.target.classList.add("in-view");
-
-          // reset will-change after animation and remove listener
-          if (willChange) {
-            el.addEventListener(
-              "transitionend",
-              function handleTransitionEnd() {
-                el.style.willChange = "auto";
-                el.removeEventListener("transitionend", handleTransitionEnd);
-              }
-            );
-          }
-
           observer.unobserve(entry.target);
         }
       });
