cab340767679722f5f569300a2476f310d594e70
Daniel Howorth <daniel.howorth@sky.com>
2025-02-19 13:40:59 +0000

    simplified close dropdown listeners and applied to social links

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,31 +1,21 @@
 import Dropdown from "../modules/dropdown.js";
 import { elementsExist } from "../modules/helpers.js";
 
-const homeBtn = document.querySelector("#home-btn");
 const headerMenuBtn = document.querySelector("#header-menu-btn");
 const headerDropdownMenu = document.querySelector(".dropdown-menu");
-const headerDropdownMenuNavLinks = document.querySelectorAll(
-  "#header-dropdown-menu nav.dropdown-nav li"
+
+const closeDropdownElements = document.querySelectorAll(
+  ".closeDropdownOnClick"
 );
 
 if (elementsExist(headerMenuBtn, headerDropdownMenu)) {
   headerMenuBtn.addEventListener("click", () => {
     Dropdown.handleDropdownBtnClick(headerMenuBtn, headerDropdownMenu);
   });
-}
 
-if (
-  elementsExist(headerDropdownMenuNavLinks, headerMenuBtn, headerDropdownMenu)
-) {
-  headerDropdownMenuNavLinks.forEach((navLink) =>
-    navLink.addEventListener("click", () =>
-      Dropdown.handleCloseDropdown(headerMenuBtn, headerDropdownMenu)
-    )
-  );
-}
-
-if (elementsExist(homeBtn, headerMenuBtn, headerDropdownMenu)) {
-  homeBtn.addEventListener("click", () =>
-    Dropdown.handleCloseDropdown(headerMenuBtn, headerDropdownMenu)
-  );
+  closeDropdownElements.forEach((element) => {
+    element.addEventListener("click", () => {
+      Dropdown.handleCloseDropdown(headerMenuBtn, headerDropdownMenu);
+    });
+  });
 }
