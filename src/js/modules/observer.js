8d5b061d98fc9f315df403fa294b1a3d3660e3f5
Daniel Howorth <daniel.howorth@sky.com>
2025-03-07 21:19:15 +0000

    removed fade up animation to improve UX

--- src/js/modules/observer.js
+++ src/js/modules/observer.js
@@ -4,18 +4,6 @@ When an observed element is in view, add the in-view class.
 Stop observing once the element is in view.
 */
 document.addEventListener("DOMContentLoaded", function () {
-  let initialScrollY = window.scrollY;
-  const initialFadeUpElements = document.querySelectorAll(".fade-up");
-
-  // If elements start at or above the intial scroll point, remove fade up animation.
-  initialFadeUpElements.forEach((el) => {
-    const rect = el.getBoundingClientRect();
-    const elementBottom = rect.bottom + window.scrollY;
-    if (initialScrollY >= elementBottom) {
-      el.classList.remove("fade-up");
-    }
-  });
-
   const observer = new IntersectionObserver(
     (entries, observer) => {
       entries.forEach((entry) => {
@@ -24,35 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         const el = entry.target;
-        let willChange = false;
-
-        // Dynamically add will-change properties before animation.
-        if (el.classList.contains("fade-up")) {
-          el.style.willChange = "opacity, transform";
-          willChange = true;
-        }
 
         el.classList.add("in-view");
-
-        // Unset will-change after animation to free up GPU resources.
-        if (willChange) {
-          el.addEventListener("transitionend", function handleTransitionEnd() {
-            el.style.willChange = "auto";
-            el.removeEventListener("transitionend", handleTransitionEnd);
-          });
-        }
 
         observer.unobserve(el);
       });
     },
     { threshold: 0.1 }
   );
-
-  // Observe fade-up elements.
-  const fadeUpElements = document.querySelectorAll(".fade-up");
-  fadeUpElements.forEach((element) => {
-    observer.observe(element);
-  });
 
   // Observe jump elements
   const jumpElements = document.querySelectorAll(".jump");
